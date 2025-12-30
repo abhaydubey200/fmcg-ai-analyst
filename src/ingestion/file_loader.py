f76e5a69dad5a788e3ae2d8f15384cbcf9cae0af
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-30 21:53:20 +0530

    Update file_loader.py

--- src/ingestion/file_loader.py
+++ src/ingestion/file_loader.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-def load_file(uploaded_file):
-    if uploaded_file.name.endswith(".csv"):
-        return pd.read_csv(uploaded_file)
-    elif uploaded_file.name.endswith(".xlsx"):
-        return pd.read_excel(uploaded_file)
-    else:
-        raise ValueError("Unsupported file format")
+
+def load_file(file):
+    try:
+        if file.name.endswith(".csv"):
+            return pd.read_csv(file)
+
+        elif file.name.endswith(".xlsx"):
+            return pd.read_excel(file, engine="openpyxl")
+
+        else:
+            raise ValueError("Unsupported file format. Upload CSV or XLSX.")
+
+    except Exception as e:
+        raise RuntimeError(f"File loading failed: {e}")
